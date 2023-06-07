2d7ea97edccb0f877d427f9893250fb8cfcbc474
Zhengyi Shen <zhengyishen1@gmail.com>
2023-06-07 18:57:50 -0400

    Input Dialog & Card Popup

--- app.py
+++ app.py
@@ -19,39 +19,98 @@ st.set_page_config(
     page_title='IgniteMe.app'
 )
 
-# load css
+# Load css
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+# Initialize state
+if 'toggle_dialog' not in st.session_state:
+    st.session_state['toggle_dialog'] = False
 
-if 'dialog_expanded' not in st.session_state:
-    st.session_state['dialog_expanded'] = False
+if 'dialog_type' not in st.session_state:
+    st.session_state['dialog_type'] = "initial"
 
-if 'form_type' not in st.session_state:
-    st.session_state['form_type'] = "inital"
+if 'goal_value' not in st.session_state:
+    st.session_state['goal_value'] = ""
+
+if 'goal' not in st.session_state:
+    st.session_state['goal'] = ""
+
+if 'user_message' not in st.session_state:
+    st.session_state['user_message'] = ""
+
+# st.success(f"goal_value: {st.session_state['goal_value']}")
 
 
 def open_dialog():
-    st.session_state['dialog_expanded'] = True
+    pass
 
 
-def clear_answer():
-    st.session_state.answer = ""
+def initial_form():
+    st.session_state['dialog_type'] = "follow-up"
+    goal = st.session_state['goal_value']
+    if goal and obstacles_1 and obstacles_2 and obstacles_3:
+        st.success(
+            f"Goal: {goal}, obstacles: {obstacles_1}, {obstacles_2}, {obstacles_3}")
+    else:
+        st.warning("Please fill all the fields")
+
+
+def follow_up_form():
+    # st.session_state['goal'] = ""
+    st.session_state['dialog_type'] = "initial"
+    st.session_state['toggle_dialog'] = False
+    if answer:
+        st.success(f"answer: {answer}")
+    else:
+        st.warning("Please fill all the fields")
 
 
 # Navbar
+if len(st.session_state['user_message']) > 0:
+    st.write(st.session_state['user_message'])
 navbar = st.container()
-logo, empty1, input, empty2, login = navbar.columns(
-    [1, 1, 3, 1, 1], gap="large")
+logo, left, input, right, login = navbar.columns([1, 1, 3, 1, 1])
 logo.image("./public/logo.png", use_column_width=True)
-empty1.empty()
-goal = input.text_input("What is your goal today",
-                        placeholder="I want to ...", key="goal", max_chars=50, on_change=open_dialog)
-empty2.empty()
 login.button("Login")
+left.empty()
+right.empty()
+
+with input.container():
+    if st.session_state['toggle_dialog']:
+        if st.session_state['dialog_type'] == "initial":
+            st.write("What stops you from achieving your goal?")
+            obstacles_1 = st.text_input("obs_1", placeholder="But...",
+                                        label_visibility="collapsed")
+            obstacles_2 = st.text_input("obs_2", value="", placeholder="And also...",
+                                        label_visibility="collapsed")
+            obstacles_3 = st.text_input("obs_3", value="", placeholder="Here is one more...",
+                                        label_visibility="collapsed")
+            st.button(
+                "Submit", on_click=initial_form)
+        else:
+            gpt_question = "hello"
+            st.write(gpt_question)
+            answer = st.text_input(
+                "answer", label_visibility="collapsed")
+            st.button("Submit", on_click=follow_up_form)
+    else:
+        input.text_input("What is your goal today", placeholder="I want to ...",
+                         key="goal", max_chars=50, on_change=open_dialog)
+
+# Card Popup
+modal = Modal("Demo Modal", key="card_modal")
+if modal.is_open():
+    with modal.container():
+        st.write("Text goes here")
+
+
+def card_popup():
+    modal.open()
+
 
 # Tabs of Categories
-categories = ["Made for you", "Your Following", "Your Posts"]
+categories = ["For You", "Following", "Your Posts"]
 grid_size = [2, 3]
 
 tab_list = st.tabs(categories)
@@ -70,57 +129,26 @@ for tab, category in zip(tab_list, categories):
                     image="",
                 )
                 if clicked_card:
-                    st.write("clicked")
+                    card_popup()
 
 
-# Open Dialog
-# goal_setting_modal = Modal(goal+" today!", key="goal-setting-modal")
+# # Once the user has submitted, upload it to the database
+# if title and url and submit:
+#     doc_ref = db.collection("posts").document(title)
+#     doc_ref.set({
+#         "title": title,
+#         "url": url
+#     })
 
 
-with st.expander('dialog_expander', expanded=st.session_state['dialog_expanded']):
-    if form_type == "inital":
-        st.write("What stops you from achieving your goal?")
-        obstacles_1 = st.text_input("Obstacles_1", placeholder="But...",
-                                    label_visibility="collapsed")
-        obstacles_2 = st.text_input("Obstacles_2", placeholder="And also...",
-                                    label_visibility="collapsed")
-        obstacles_3 = st.text_input("Obstacles_3", placeholder="Here is one more...",
-                                    label_visibility="collapsed")
+# # And then render each post, using some light Markdown
+# posts_ref = db.collection("posts")
+# refs = posts_ref  # .where('title', '==', 'Apple')
 
-        # Every form must have a submit button.
-        submitted = st.button("Submit")
-        if submitted:
-            st.write("goal: ", goal, "obstacles_1: ", obstacles_1,
-                     "obstacles_2: ", obstacles_2, "obstacles_3: ", obstacles_3)
-            form_type = "Submitted"
-    else:
-        gpt_question = "hello"
-        st.write(gpt_question)
-        answer = st.text_input(
-            "answer", label_visibility="collapsed")
-        submitted = st.button("Submit")
+# for doc in refs.stream():
+#     post = doc.to_dict()
+#     title = post["title"]
+#     url = post["url"]
 
-
-# if goal_setting_modal.is_open():
-#     with goal_setting_modal.container():
-#         if form_type == "inital":
-#             st.write("What stops you from achieving your goal?")
-#             obstacles_1 = st.text_input("Obstacles_1", placeholder="But...",
-#                                         label_visibility="collapsed")
-#             obstacles_2 = st.text_input("Obstacles_2", placeholder="And also...",
-#                                         label_visibility="collapsed")
-#             obstacles_3 = st.text_input("Obstacles_3", placeholder="Here is one more...",
-#                                         label_visibility="collapsed")
-
-#             # Every form must have a submit button.
-#             submitted = st.button("Submit")
-#             if submitted:
-#                 st.write("goal: ", goal, "obstacles_1: ", obstacles_1,
-#                          "obstacles_2: ", obstacles_2, "obstacles_3: ", obstacles_3)
-#                 form_type = "Submitted"
-#         else:
-#             gpt_question = "hello"
-#             st.write(gpt_question)
-#             answer = st.text_input(
-#                 "answer", label_visibility="collapsed")
-#             submitted = st.button("Submit")
+#     st.subheader(f"Post: {title}")
+#     st.write(f":link: [{url}]({url})")
