7b07eec1be614af03399d0139867dcc2beb8f9c4
Zhengyi Shen <zhengyishen1@gmail.com>
2023-06-10 22:18:49 -0400

    update user auth

--- app.py
+++ app.py
@@ -2,16 +2,21 @@ import json
 from google.cloud import firestore
 from google.oauth2 import service_account
 import streamlit as st
-from streamlit_modal import Modal
-from streamlit_card import card
-from streamlit_chat import message
+# from streamlit_modal import Modal
 from gpt_api import GPT_API
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+from firebase_admin import firestore
 
 
 # Securely connect to Firebase
-key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds)
+if not firebase_admin._apps:
+    key_dict = json.loads(st.secrets["textkey"])
+    # creds = service_account.Credentials.from_service_account_info(key_dict)
+    cred = credentials.Certificate("firestore-key.json")
+    firebase_admin.initialize_app(cred)
+db = firestore.client()
 batch = db.batch()
 
 
@@ -28,20 +33,18 @@ with open('style.css') as f:
 # Initialise session state variables
 session_states = ['goal_value', 'goal', 'user_message', "user_answer",
                   'user_goal', 'content', 'gpt_coach', 'gpt_response',
-                  'obstacles_1', 'obstacles_2', 'obstacles_3', 'dialog_type', 'post_id', 'user_input', 'user_name', 'obstacle_id', 'obstacle_value']
+                  'obstacles_1', 'obstacles_2', 'obstacles_3', 'dialog_type', 'post_id', 'user_input', 'obstacle_id', 'obstacle_value', 'user_info', 'auth_info', 'gpt_response']
 for state in session_states:
     if state not in st.session_state:
         st.session_state[state] = ""
 
-session_states = ['expand_post', 'toggle_dialog']
+session_states = ['expand_post', 'toggle_dialog', 'authentication', 'post_id']
 for state in session_states:
     if state not in st.session_state:
         st.session_state[state] = False
 
 
 # Define Components
-
-
 def update_firebase(collection, data):
     doc_ref = db.collection(collection).document()
     doc_ref.set(data)
@@ -51,6 +54,50 @@ def stream_firebase(collection):
     post_refs = db.collection(collection)
     docs = post_refs.stream()
     return docs
+
+
+def signup():
+    auth_info = st.session_state['auth_info']
+    try:
+        user = auth.create_user(email=auth_info['email'])
+
+        user_info = {
+            "user_email": auth_info['email'], "user_name": auth_info['user_name']}
+        user_ref = db.collection('users').document(user.uid)
+        user_ref.set(user_info)
+        st.session_state['user_info'] = user_info
+        st.success('Welcome! ' + user_info['user_name'])
+        st.session_state['authentication'] = False
+        if st.session_state['gpt_response']:
+            post_goal()
+    except auth.EmailAlreadyExistsError:
+        st.sidebar.error(
+            'Email already exists. Please try a different email.')
+    except Exception as e:
+        st.sidebar.error(
+            'Account creation failed. Please try again later.')
+        st.write(e)
+
+
+def login():
+    auth_info = st.session_state['auth_info']
+    try:
+        user = auth.get_user_by_email(auth_info['email'])
+        user_ref = db.collection('users').document(user.uid)
+        user_info = user_ref.get().to_dict()
+        st.session_state['user_info'] = user_info
+        st.session_state['user_name'] = user_info['user_name']
+        st.success('Welcome back! ' + user_info['user_name'])
+        st.session_state['authentication'] = False
+        if st.session_state['gpt_response']:
+            post_goal()
+    except auth.UserNotFoundError:
+        # st.sidebar.error('Invalid email or password. Please try again.')
+        signup()
+
+
+def logout():
+    st.session_state['user_info'] = ""
 
 
 def open_dialog():
@@ -95,12 +142,25 @@ def initial_form():
         gpt_response = gpt_coach.chat(user_input + clarification)
         st.session_state['gpt_coach'] = gpt_coach
 
-        gpt_response = json.loads(gpt_response)
         print(gpt_response)
+        gpt_response = json.loads(gpt_response)
         st.session_state['gpt_response'] = gpt_response['response']
 
     else:
         st.warning("Please fill all the fields")
+
+
+def post_goal():
+    gpt_response = st.session_state['gpt_response']
+    gpt_response['goal']['user_info'] = st.session_state['user_info']
+    user_ref = db.collection('posts').document()
+    batch.set(user_ref, gpt_response['goal'])
+    for obstacle_value in gpt_response['obstacles']:
+        subcollection_ref = user_ref.collection(
+            'obstacles').document()
+        batch.set(subcollection_ref, obstacle_value)
+    batch.commit()
+    st.session_state['gpt_response'] = ""
 
 
 def follow_up_form():
@@ -132,20 +192,18 @@ def follow_up_form():
         gpt_response = gpt_coach.chat(answer + instructions)
         st.session_state['gpt_coach'] = gpt_coach
 
-        gpt_response = json.loads(gpt_response)
         print(gpt_response)
+        gpt_response = json.loads(gpt_response)
 
         if gpt_response['success']:
+            st.session_state['gpt_response'] = gpt_response
             st.session_state['toggle_dialog'] = False
-            st.session_state['dialog_type'] = ""
+            st.session_state['dialog_type'] = ''
 
-            user_ref = db.collection('posts').document()
-            gpt_response['goal']['user_name'] = st.session_state['user_name']
-            batch.set(user_ref, gpt_response['goal'])
-            for obstacle_value in gpt_response['obstacles']:
-                subcollection_ref = user_ref.collection('obstacles').document()
-                batch.set(subcollection_ref, obstacle_value)
-            batch.commit()
+            if st.session_state['user_info']:
+                post_goal()
+            else:
+                st.session_state['authentication'] = True
         else:
             st.session_state['gpt_response'] = gpt_response['response']
             st.session_state['user_answer'] = ""
@@ -163,7 +221,7 @@ def card_grid(items_per_col):
             post['id'] = doc.id
 
             st.header(post["content"])
-            st.caption(post["user_name"])
+            st.caption(post['user_info']["user_name"])
             clicked = st.button('View', key=i)
             if clicked:
                 st.session_state['user_goal'] = post["content"]
@@ -174,36 +232,47 @@ def card_grid(items_per_col):
 
 
 def navbar():
-    if len(st.session_state['user_message']) > 0:
-        st.write(st.session_state['user_message'])
     navbar = st.container()
-    logo, left, input, right, login = navbar.columns([1, 1, 3, 1, 1])
+    logo, left, center, right, button = navbar.columns([1, 1, 3, 1, 1])
     logo.image("./public/logo.png", use_column_width=True)
-    login.button("Login")
     left.empty()
     right.empty()
 
-    with input.container():
-        if st.session_state['toggle_dialog']:
-            if st.session_state['dialog_type'] == "":
-                st.write("What stops you from achieving your goal?")
-                st.text_input("obstacles_1", key="obstacles_1", placeholder="But...",
-                              label_visibility="collapsed")
-                st.text_input("obstacles_2", key="obstacles_2", value="", placeholder="And also...",
-                              label_visibility="collapsed")
-                st.text_input("obstacles_3", key="obstacles_3", value="", placeholder="Here is one more...",
-                              label_visibility="collapsed")
-                st.button(
-                    "Submit", on_click=initial_form)
+    with button:
+        if not st.session_state['user_info']:
+            if not st.session_state['authentication']:
+                clicked = button.button('Account')
+                if clicked:
+                    st.session_state['authentication'] = True
             else:
-                st.write(st.session_state['gpt_response'])
-                st.text_input(
-                    "answer", key="user_answer", label_visibility="collapsed")
-                st.button("Submit", on_click=follow_up_form)
+                button.button('Sign in', on_click=login)
         else:
-            input.text_input("What is your goal today", placeholder="I want to ...",
+            button.button(
+                st.session_state['user_info']['user_name'], on_click=logout)
 
-                             key="goal", max_chars=50, on_change=open_dialog)
+    with center.container():
+        if st.session_state['authentication']:
+            authenticate()
+        else:
+            if st.session_state['toggle_dialog']:
+                if st.session_state['dialog_type'] == "":
+                    st.write("What stops you from achieving your goal?")
+                    st.text_input("obstacles_1", key="obstacles_1", placeholder="But...",
+                                  label_visibility="collapsed")
+                    st.text_input("obstacles_2", key="obstacles_2", value="", placeholder="And also...",
+                                  label_visibility="collapsed")
+                    st.text_input("obstacles_3", key="obstacles_3", value="", placeholder="Here is one more...",
+                                  label_visibility="collapsed")
+                    st.button(
+                        "Submit", on_click=initial_form)
+                else:
+                    st.write(st.session_state['gpt_response'])
+                    st.text_input(
+                        "answer", key="user_answer", label_visibility="collapsed")
+                    st.button("Submit", on_click=follow_up_form)
+            else:
+                center.text_input("What is your goal today", placeholder="I want to ...",
+                                  key="goal", max_chars=50, on_change=open_dialog)
     navbar.divider()
 
 
@@ -211,8 +280,9 @@ def submit_message():
     collection = "posts/{}/obstacles/{}/messages".format(
         st.session_state['post_id'], st.session_state['obstacle_id'])
     data = {"content": st.session_state['user_input'],
-            "user": st.session_state['user_name']}
+            "user_info": st.session_state['user_info']}
     update_firebase(collection, data)
+    st.session_state['user_input'] = ""
 
 
 def post_expander():
@@ -241,7 +311,7 @@ def post_expander():
                         data = doc.to_dict()
 
                         name, content, space = st.columns([2, 5, 2])
-                        name.write(data['user'])
+                        name.write(data['user_info']['user_name'])
                         content.write(data['content'])
                         space.empty()
 
@@ -253,11 +323,14 @@ def post_expander():
                     st.empty()
 
 
-login = False
-if login:
-    st.session_state['user_name'] = "123"
-else:
-    st.session_state['user_name'] = "Anonymous"
+def authenticate():
+    left, right = st.columns(2)
+    with left:
+        user_name = st.text_input('Please input your user name')
+    with right:
+        email = st.text_input('Please enter your email address')
+    st.session_state['auth_info'] = {'user_name': user_name, 'email': email}
+
 
 navbar()
 card_grid(3)
